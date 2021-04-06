0f8cbea03c2f65f957e33ee5214ace58dc319e52
Alfredo Tlahuel <Vfredo602@gmail.com>
2021-04-06 07:27:31 -0500

    added character variables

--- script.js
+++ script.js
@@ -1,3 +1,90 @@
+//Create variables to store characters types, and create global variables//
+var upperCaseChar = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+var lowerCaseChar = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var specialChars = [
+  "@",
+  "%",
+  "+",
+  ",",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  ".",
+];
+
+var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -5,13 +92,14 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   // var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  
 
   passwordText.value = password;
-  var lenghtOfPassword = parseInt(prompt("You must enter 8 characters including special characters, a number and a capital letter"));
- document.getElementById('password')
-  
-
+  var lenghtOfPassword = parseInt(
+    prompt(
+      "You must enter at least 8 characters and no more than 128 including special characters, a number and a capital letter"
+    )
+  );
+  document.getElementById("password");
 }
 
 // Add event listener to generate button
